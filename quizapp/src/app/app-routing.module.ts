a94c2278d51d6bb75992cae129960a9200bd7e26
Kisavi <deniskisavi@gmail.com>
2022-08-18 05:29:35 +0800

    create a question section that allows users to take questions of a particular quiz

--- quizapp/src/app/app-routing.module.ts
+++ quizapp/src/app/app-routing.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddQuestionComponent } from './components/add-question/add-question.component';
 import { CreateQuizComponent } from './components/create-quiz/create-quiz.component';
+import { QuizQuestionsComponent } from './components/quiz-questions/quiz-questions.component';
 import { QuizzesComponent } from './components/quizzes/quizzes.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/quizzes', pathMatch: 'full' },
   { path: 'quizzes', component: QuizzesComponent },
   { path: 'create-quiz', component: CreateQuizComponent },
-  { path: 'add-question', component: AddQuestionComponent }
+  { path: 'add-question', component: AddQuestionComponent },
+  { path: 'quiz-questions', component: QuizQuestionsComponent },
 ];
 
 @NgModule({
--- quizapp/src/app/app.module.ts
+++ quizapp/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { QuizzesComponent } from './components/quizzes/quizzes.component';
 import { CreateQuizComponent } from './components/create-quiz/create-quiz.component';
 import { AddQuestionComponent } from './components/add-question/add-question.component';
+import { QuizQuestionsComponent } from './components/quiz-questions/quiz-questions.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { AddQuestionComponent } from './components/add-question/add-question.com
     NavbarComponent,
     QuizzesComponent,
     CreateQuizComponent,
-    AddQuestionComponent
+    AddQuestionComponent,
+    QuizQuestionsComponent
   ],
   imports: [
     BrowserModule,
